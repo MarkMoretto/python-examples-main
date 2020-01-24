580f04ad12e306b9988907b8cdd91564189abf30
Mark Moretto <otteromkram@gmail.com>
2020-01-24 15:22:48 -0500

    Create desktop_windows.py

---
+++ os-operations/desktop_windows.py
@@ -0,0 +1,132 @@
+
+"""
+Purpose: Restore window positioning after waking from sleep
+Date created: 2020-01-24
+
+https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentthreadid
+
+Contributor(s):
+    Mark M.
+"""
+
+import re
+import ctypes as C
+from ctypes import wintypes
+
+
+# usr32_dll = r"C:\Windows\System32\user32.dll"
+# kern32_dll = r"C:\Windows\System32\kernel32.dll"
+# usr32 = C.windll.LoadLibrary(usr32_dll)
+# kern32 = C.windll.LoadLibrary(kern32_dll)
+
+k32 = C.WinDLL("kernel32", use_last_error=True)
+user32 = C.WinDLL("user32", use_last_error=True)
+
+
+# ### Misc
+# k32.GetCurrentThreadId.restype = wtypes.DWORD
+# curr_thread = k32.GetCurrentThreadId()
+
+# user32.GetThreadDesktop.restype = wtypes.HDESK
+# user32.GetThreadDesktop.argtypes = [wtypes.DWORD]
+# desktop_thread = user32.GetThreadDesktop(curr_thread)
+
+# k32.GetModuleHandleW.argtypes = [wtypes.LPCWSTR]
+# k32.GetModuleHandleW.restype = wtypes.HMODULE
+
+# hMod = k32.GetModuleHandleW("kernel32.dll")
+
+
+
+
+### Window Information and Placement ###
+
+struct_string ="""
+typedef struct tagWINDOWPLACEMENT {
+  UINT  length;
+  UINT  flags;
+  UINT  showCmd;
+  POINT ptMinPosition;
+  POINT ptMaxPosition;
+  RECT  rcNormalPosition;
+  RECT  rcDevice;
+} WINDOWPLACEMENT;
+"""
+
+def process_struct(input_str):
+    struct_tokens = [i.strip() for i in input_str.strip().split('\n') if not i.startswith(('typedef','}',))]
+    struct_wintypes = [re.split(r"\s+", i)[0] for i in struct_tokens]
+    struct_fields = [re.split(r"\s+", i)[1].replace(';','') for i in  struct_tokens]
+    frmt_list = [f'\t\t("{i[1]}", wintypes.{i[0]})' for i in list(zip(struct_wintypes, struct_fields))]
+    print(',\n'.join(frmt_list))
+
+# process_struct(struct_string)
+
+
+class WINDOWINFO(C.Structure):
+    __fields__ = [
+            ("cbSize", wintypes.DWORD),
+            ("rcWindow", wintypes.RECT),
+            ("rcClient", wintypes.RECT),
+            ("dwStyle", wintypes.DWORD),
+            ("dwExStyle", wintypes.DWORD),
+            ("dwWindowStatus", wintypes.DWORD),
+            ("cxWindowBorders", wintypes.UINT),
+            ("cyWindowBorders", wintypes.UINT),
+            ("atomWindowType", wintypes.ATOM),
+            ("wCreatorVersion", wintypes.WORD)
+            ]
+
+windowinfo = WINDOWINFO()
+ptr_windowinfo = C.pointer(windowinfo)
+
+
+
+class WINDOWPLACEMENT(C.Structure):
+    __fields__ = [
+                ("length", wintypes.UINT),
+                ("flags", wintypes.UINT),
+                ("showCmd", wintypes.UINT),
+                ("ptMinPosition", wintypes.POINT),
+                ("ptMaxPosition", wintypes.POINT),
+                ("rcNormalPosition", wintypes.RECT),
+                ("rcDevice", wintypes.RECT)
+                ]
+
+
+
+windowplacement = WINDOWPLACEMENT()
+windowplacement.length = C.sizeof(WINDOWPLACEMENT)
+
+
+#--- Get window HWND
+# user32.GetForegroundWindow.restype = wintypes.HWND
+# focus_window = user32.GetForegroundWindow()
+
+user32.GetDesktopWindow.restype = wintypes.HWND
+hWnd = user32.GetDesktopWindow()
+
+#--- Set pointer
+# ptr_windowplacement = C.POINTER(windowplacement)
+
+
+user32.GetWindowPlacement.argtypes = [wintypes.HWND, C.POINTER(WINDOWPLACEMENT)]
+user32.GetWindowPlacement.restype = wintypes.BOOL
+
+if user32.GetWindowPlacement(hWnd, windowplacement) == 0:
+    print("GetWindowPlacement failed to process.")
+
+
+
+
+
+
+
+
+user32.GetForegroundWindow.restype = wintypes.HWND
+focus_window = user32.GetForegroundWindow()
+
+
+
+
+
