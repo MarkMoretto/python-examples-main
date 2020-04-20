508ac8e202c149adbaba0e5f7afa370c7b344b68
Mark Moretto <otteromkram@gmail.com>
2020-04-20 14:31:02 -0400

    not needed

--- notebook-samples/spark/adv-data-sci-IBM/config.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    "NOTEBOOKS_DIR",
-    "PARQUET_FILE",
-    ]
-
-import os
-
-NOTEBOOKS_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.getcwd())))
-PARQUET_FILE = rf"{NOTEBOOKS_DIR}\washing.parquet"
