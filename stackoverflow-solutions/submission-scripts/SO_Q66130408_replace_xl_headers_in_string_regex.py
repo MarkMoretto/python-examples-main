26d2f51f87fd39e54d37425f597bbbdcefa5d847
Mark M <otteromkram@gmail.com>
2021-02-09 21:48:52 -0600

    Create SO_Q66130408_replace_xl_headers_in_string_regex.py

---
+++ stackoverflow-solutions/submission-scripts/SO_Q66130408_replace_xl_headers_in_string_regex.py
@@ -0,0 +1,32 @@
+
+"""
+Purpose: Stackoverflow response
+Date created: 2021-02-09
+
+URL: https://stackoverflow.com/questions/66130408/how-do-i-replace-the-column-headers-in-a-string-with-a-list-of-replacement-colum/66130639#66130639
+
+Contributor(s):
+    Mark M.
+"""
+
+
+import re
+oldcolumn = '''Table.TransformColumnTypes(#"Promoted Headers",{{"ID_ORD", type text}, {"CDRNOR", type text}, {"DT_ORD", type datetime}, {"FLSTAT", Int64.Type},.....'''
+newcolumn = '''CDUNOD   CDTIDO  CDRNOR  DT_ORD....'''
+newcolumnList = newcolumn.split()
+# originalcolumnsRegex = re.compile(r'\{\"\w+')
+
+ptrn = r'{"([A-Z_]+?)"'
+p = re.compile(ptrn)
+
+# p.findall(oldcolumn)
+
+
+for o, n in zip(p.findall(oldcolumn), newcolumnList):
+    oldcolumn = oldcolumn.replace(o, n)
+
+re.sub(ptrn, oldcolumn,)
+
+for i in newcolumnList
+      originalcolumnsRegex.sub(r'{"'newcolumnList[i],oldcolumn)
+      i = i +1
