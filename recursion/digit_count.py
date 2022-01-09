e8bb70510395ba9edd36ade73e7284c3f17a401f
Mark Moretto <otteromkram@gmail.com>
2022-01-09 13:45:23 -0600

    Update digit_count.py
    Reworked main function to include subfunction and some type checks.

--- recursion/digit_count.py
+++ recursion/digit_count.py
@@ -1,43 +1,73 @@
 #!/bin/python
 
 from __future__ import annotations
-from typing import Any
 
-def is_number(s: Any) -> bool:
-    """Returns True if string is a number.
+def is_number(s: str) -> bool:
+    """ Returns True is string is a number, else False.
     """
-    return str(s).replace(".", "",1).isdigit()
+    return s.replace(".", "",1).isdigit()
 
-def size(number: int, __size: int = 0) -> int:
-    """Return count of digits in decimal number.  This does NOT return
+
+def is_float(s: str) -> bool:
+    """Returns True if string represents floating-point number, else False.
+    """
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+    
+def size(number: (int | float), __size: int = 0) -> int:
+    """Return count of digits in decimal number.  This does not return
     the number of bits or memory allocation to the variable.
 
     Parameters
-    ---------
+    ----------
     number : int
         Decimal number to process.
 
     Returns
     -------
     int
-        Size of decimal number (how many digits it has).
+        Size of decimal number (how many digits it has).  If non numeric value
+        passed, then -1 will be returned.
 
-    Example
-    -------
-    >>> a = 123
-    >>> size(a)
-    3
-    >>> size("a")
+    Examples
+    --------
+    >>> size(1111)
+    4
+    >>> size("1111")
+    4
+    >>> size(-1111)
+    4
+    >>> size("abcd")
+    -1
+    >>> size("")
     -1
     """
 
-    # Check if value is numeric, then continue.
-    if is_number(number):
-        if number:
-            return size(number//10, __size+1)
-        return __size
-    return -1
+    def _sz(n, i: int = 0) -> int:
+        """Inner function that calculates size of numeric value."""
+        if n:
+            return _sz(n//10, i+1)
+        return i
+
+    if isinstance(number, str) and is_number(number):
+        if is_float(number):
+            number = float(number)
+        else:
+            number = int(number)
+        
+    if isinstance(number, (int, float)):
+        # Use abs() to handle negative values.
+        return _sz(abs(number))
+    
+    else:
+        return -1
+
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+
