f08f2fe05f30ffc0f2fe369bc1f4da3564702fdf
Mark Moretto <25012707+MarkMoretto@users.noreply.github.com>
2023-03-14 09:17:29 -0500

    Create pylint_directives_header.py
    Just a file to store some pylint directives.

---
+++ general-utilities/pylint_directives_header.py
@@ -0,0 +1,11 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=too-few-public-methods
+# pylint: disable=no-member
+# pylint: disable=single-string-used-for-slots
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=bad-classmethod-argument
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=logging-not-lazy
+# pylint: disable=super-init-not-called
+# pylint: disable=no-else-raise
